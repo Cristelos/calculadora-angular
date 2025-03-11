d987bdf4eab2fec104fc6baa5cbc8e77f60a016e
Cristelos <cristelos@msn.com>
2025-03-11 12:40:22 +0100

    Guardando avance

--- src/app/calculator/components/calculator/calculator.component.ts
+++ src/app/calculator/components/calculator/calculator.component.ts
@@ -1,13 +1,58 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  inject,
+  viewChildren,
+} from '@angular/core';
 import { CalculatorButtonComponent } from '../calculator-button/calculator-button.component';
+import { CalculatorService } from '@/calculator/services/calculator.service';
 
 @Component({
   selector: 'calculator',
   standalone: true,
-  imports: [
-    CalculatorButtonComponent,
-  ],
+  imports: [CalculatorButtonComponent],
   templateUrl: './calculator.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {
+    '(document:keyup)': 'handleKeyboardEvent($event)',
+  },
 })
-export class CalculatorComponent { }
+export class CalculatorComponent {
+  private calculatorService = inject(CalculatorService);
+
+  public calculatorButtons = viewChildren(CalculatorButtonComponent);
+
+  // get resultText(){
+  //   return this.calculatorService.resultText;
+  // }
+
+  public resultText = computed(() => this.calculatorService.resultText());
+  public subResultText = computed(() => this.calculatorService.subResultText());
+  public lastOperator = computed(() => this.calculatorService.lastOperator());
+
+  handleClick(key: string) {
+   // console.log({ key });
+    this.calculatorService.constructNumber(key);
+  }
+
+  // @HostListener('document:keyup', ['$event'])
+  handleKeyboardEvent(event: KeyboardEvent) {
+    const keyEquivalents: Record<string, string> = {
+      Enter: '=',
+      Clear: 'C',
+      Escape: 'C',
+      X: '*',
+      '/': '÷',
+    };
+
+    const key = event.key;
+    const keyValue = keyEquivalents[key] ?? key;
+
+    this.handleClick(keyValue);
+
+    this.calculatorButtons().forEach((button) => {
+      button.keyboardPressedStyle(keyValue);
+    });
+  }
+}
